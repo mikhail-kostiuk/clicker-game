939622da763fc94d6cad90fc8f9cebf714d6f1d5
Mykhailo Kostiuk <mykhailo_kostiuk@epam.com>
2020-04-23 13:14:48 +0300

    Move pages to pages directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { HomeComponent } from './home/home.component';
+import { GameComponent } from './pages/game/game.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
