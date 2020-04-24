099a7a966758036f2b52cd2e4543559de1c0b0ce
Mykhailo Kostiuk <mykhailo_kostiuk@epam.com>
2020-04-24 16:58:59 +0300

    Refactor game page

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,47 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { GameService } from 'src/app/shared/services/game.service';
+import { TimerService } from 'src/app/shared/services/timer.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class GameComponent implements OnInit {
-  username: string;
+export class GameComponent implements OnInit, OnDestroy {
+  state: string;
   clicks: number;
   time: number;
-  state: string = 'READY';
-  timer: number;
+  subscriptions: Subscription[] = [];
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.username = JSON.parse(localStorage.getItem('username'));
-    this.newGame();
+  constructor(
+    private gameService: GameService,
+    private timerService: TimerService,
+    private router: Router
+  ) {
+    const gameStateSub = gameService.stateChange$.subscribe((state) => {
+      this.state = state;
+      if (state === 'FINISHED') {
+        this.router.navigate(['result']);
+      }
+    });
+    const timerTimeSub = timerService.timeChange$.subscribe((time) => {
+      this.time = time;
+      if (time === 0) {
+        this.gameService.endGame();
+      }
+    });
+    this.subscriptions.push(gameStateSub, timerTimeSub);
   }
 
-  newGame() {
-    this.clicks = 0;
-    this.time = 10;
-    this.state = 'READY';
+  ngOnInit(): void {
+    this.state = this.gameService.getState();
+    this.clicks = this.gameService.getClicks();
+    this.time = this.gameService.getTime();
+  }
+
+  ngOnDestroy(): void {
+    this.gameService.endGame();
+    this.timerService.stopTimer();
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
   startGame() {
-    this.state = 'RUNNING';
-    this.timer = setInterval(() => {
-      if (this.time > 0) {
-        this.time--;
-      } else {
-        this.endGame();
-      }
-    }, 1000);
+    this.gameService.startGame();
+    this.timerService.setInitialTime(this.time);
+    this.timerService.resetTimer();
+    this.timerService.startTimer();
+    console.log(this.state);
   }
 
-  endGame() {
-    this.state = 'FINISHED';
-    clearInterval(this.timer);
+  pauseGame() {
+    this.gameService.pauseGame();
+    this.timerService.pauseTimer();
   }
 
-  click() {
-    this.clicks++;
+  resumeGame() {
+    this.gameService.startGame();
+    this.timerService.startTimer();
+  }
+
+  onClick() {
+    this.gameService.click();
+    this.clicks = this.gameService.getClicks();
   }
 }
