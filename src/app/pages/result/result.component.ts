0d0b3891ba0f5029f72d538c5e2c715f8a29f3be
Mykhailo Kostiuk <mykhailo_kostiuk@epam.com>
2020-04-24 16:58:37 +0300

    Add result page

---
+++ src/app/pages/result/result.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/shared/services/game.service';
+import { User } from 'src/app/shared/models/user.model';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.scss'],
+})
+export class ResultComponent implements OnInit {
+  user: User;
+  clicks: number;
+
+  constructor(
+    private router: Router,
+    private gameService: GameService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
+    this.clicks = this.gameService.getClicks();
+  }
+
+  newGame() {
+    this.gameService.newGame();
+    this.router.navigate(['/game']);
+  }
+}
